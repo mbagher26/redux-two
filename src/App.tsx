5cc6b66cfdb92b4508dcab624e12795e4c47f4bc
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-05 09:51:08 +0330

    import AddPostForm in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import PostsList from './features/posts/PostList';
-
+import AddPostForm from './features/posts/AddPostForm'
 
 function App() {
 
   return (
     <main>
       <PostsList/>
+      <AddPostForm/>
     </main>
   )
 }
