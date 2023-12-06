29393307728a0745433144834e4c09f0c1487f05
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-06 09:29:17 +0330

    wrap PostAuthor in  p tag

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -2,20 +2,22 @@ import "./post.css";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
-const PostsList = () =>{
+const PostsList = () => {
 
     const posts = useSelector(selectAllPosts)
 
-    const renderedPosts = posts.map((post) =>(
+    const renderedPosts = posts.map((post) => (
 
         <article className="post-article" key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
-            <PostAuthor userId={post.id}/>
+            <p>
+                <PostAuthor userId={post.id} />
+            </p>
         </article>
     ))
 
-    return(
+    return (
         <section className="post">
             <h2>Posts</h2>
             {renderedPosts}
