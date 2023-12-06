24cd4ba09936ea11f3d1c5f46d73af873370816a
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-06 10:49:09 +0330

    add date

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,14 +1,25 @@
 import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
+import sub from "date-fns/sub";
 export interface postState {
     id: string,
     title: string,
-    content: string
+    content: string,
+    date: string
 }
 
 const initialState: postState[] = [
-    { id: '1', title: 'Learning Redux Tolkit', content: 'I`ve heard go things.' },
-    { id: '2', title: 'Slices...', content: "The more I say slice, the more I want pizza." }
+    {
+    id: '1', 
+    title: 'Learning Redux Tolkit', 
+    content: 'I`ve heard go things.',
+    date: sub(new Date(), {minutes: 10}).toISOString()
+},
+    { id: '2', 
+    title: 'Slices...', 
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), {minutes: 5}).toISOString()
+}
 ]
 
 const postsSlice = createSlice({
@@ -25,7 +36,8 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
-                        userId
+                        userId,
+                        date: new Date().toISOString()
                     }
                 };
             }
