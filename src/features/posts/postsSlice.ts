b0a48fc6199cd438191fc49204155e647c7474bb
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-09 11:58:03 +0330

    update initialState and typeString code

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,12 +1,12 @@
 import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
-import sub from "date-fns/sub";
-export interface postState {
+
+export interface post {
     id: string,
     title: string,
     content: string,
-    date: string,
     userId: string,
+    date: string,
     reactions: {
         thumbsUp: number,
         wow: number;
@@ -16,44 +16,25 @@ export interface postState {
         [key:string] : number
     }
 }
-
-const initialState: postState[] = [
-    {
-    id: '1', 
-    title: 'Learning Redux Tolkit', 
-    content: 'I`ve heard go things.',
-    userId: '',
-    date: sub(new Date(), {minutes: 10}).toISOString(),
-    reactions : {
-        thumbsUp: 0,
-        wow: 0,
-        heart: 0,
-        rocket: 0,
-        coffee: 0
-    } 
-    },
-    { id: '2', 
-    title: 'Slices...', 
-    userId:'',
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), {minutes: 5}).toISOString(),
-    reactions : {
-        thumbsUp: 0,
-        wow: 0,
-        heart: 0,
-        rocket: 0,
-        coffee: 0
-    } 
+export interface postState {
+    posts: post [] ,
+    status: string ,
+    error:string | null 
 }
-]
+
+const initialState: postState  = {
+    posts: [],
+    status: 'idle',
+    error: null
+}
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
         postAdded: {
-            reducer(state:postState[], action:PayloadAction<postState>) {
-                state.push(action.payload)
+            reducer(state, action:PayloadAction<any>) {
+                state.posts.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
@@ -77,7 +58,7 @@ const postsSlice = createSlice({
         reactionAdded(state, action:PayloadAction<{id:string , reaction:string}>){
 
             const {id, reaction} = action.payload
-            const existingPost = state.find((post) => post.id === id)
+            const existingPost = state.posts.find((post) => post.id === id)
 
             if(existingPost){
                 existingPost.reactions[reaction]++
